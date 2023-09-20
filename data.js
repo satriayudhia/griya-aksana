8815f162ca501bcd0daf86e00800dae38bee2df7
satriayudhia <satria.yudhiap@gmail.com>
2023-09-20 14:35:41 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,571 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1f---view-perumahan",
+      "name": "1F - VIEW PERUMAHAN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.5032426496082003,
+        "pitch": 0.12967425527722654,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2604847390019813,
+          "pitch": -0.0207668794834035,
+          "rotation": 0,
+          "target": "1-1a---lt1---hal-depan"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1a---lt1---hal-depan",
+      "name": "1A - LT1 - HAL DEPAN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.27147421565143404,
+        "pitch": 0.19364551930092588,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5102674933441396,
+          "pitch": 0.2761352444010061,
+          "rotation": 0,
+          "target": "0-1f---view-perumahan"
+        },
+        {
+          "yaw": -0.19350590297039183,
+          "pitch": 0.09560027549061445,
+          "rotation": 0,
+          "target": "2-1b---lt1---ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1b---lt1---ruang-utama",
+      "name": "1B - LT1 - RUANG UTAMA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.06620394876907554,
+        "pitch": 0.21688978180320895,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9053553686528879,
+          "pitch": 0.1351133462846832,
+          "rotation": 0,
+          "target": "1-1a---lt1---hal-depan"
+        },
+        {
+          "yaw": 0.21192586238094435,
+          "pitch": 0.19451405163539803,
+          "rotation": 0,
+          "target": "3-1c---lt-1---wc"
+        },
+        {
+          "yaw": 1.2670995953637032,
+          "pitch": 0.19440837932627986,
+          "rotation": 0,
+          "target": "4-1d---lt1---ruang-belakang"
+        },
+        {
+          "yaw": -0.019347626898367665,
+          "pitch": -0.2483987223078543,
+          "rotation": 0,
+          "target": "6-2a---lt2---ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1c---lt-1---wc",
+      "name": "1C - LT 1 - WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.931424828848396,
+        "pitch": 0.361734164413928,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4929581445639961,
+          "pitch": 0.16163253048979342,
+          "rotation": 0,
+          "target": "2-1b---lt1---ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-1d---lt1---ruang-belakang",
+      "name": "1D - LT1 - RUANG BELAKANG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0803375241857562,
+        "pitch": 0.19832698932934534,
+        "fov": 1.3780365151013685
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.019509862270395,
+          "pitch": 0.22980499498664564,
+          "rotation": 0.7853981633974483,
+          "target": "5-1e---lt1---ruang-art"
+        },
+        {
+          "yaw": 2.534912358266279,
+          "pitch": 0.25046513697208184,
+          "rotation": 10.995574287564278,
+          "target": "2-1b---lt1---ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-1e---lt1---ruang-art",
+      "name": "1E - LT1 - RUANG ART",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.09668663832337,
+        "pitch": 0.6138934009364245,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0004363305713805943,
+          "pitch": 0.2695060183479505,
+          "rotation": 0,
+          "target": "4-1d---lt1---ruang-belakang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2a---lt2---ruang-utama",
+      "name": "2A - LT2 - RUANG UTAMA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.9900929139406998,
+        "pitch": 0.18276225238153643,
+        "fov": 1.3773494635633166
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0500467509277502,
+          "pitch": 0.21986024119637904,
+          "rotation": 0,
+          "target": "10-2e---lt2---wc-1"
+        },
+        {
+          "yaw": -0.3613407308762504,
+          "pitch": 0.2049117353466805,
+          "rotation": 0,
+          "target": "7-2b---lt2---ruang-dalam-1"
+        },
+        {
+          "yaw": -1.3087557415694206,
+          "pitch": 0.3374612126462395,
+          "rotation": 0,
+          "target": "9-2d---lt2---ruang-dalam-2"
+        },
+        {
+          "yaw": 2.2231216577803163,
+          "pitch": 0.2735023515050603,
+          "rotation": 0,
+          "target": "11-2f---lt2---ruang-dalam-3"
+        },
+        {
+          "yaw": 0.6103057374879342,
+          "pitch": 0.6879732547771358,
+          "rotation": 3.141592653589793,
+          "target": "2-1b---lt1---ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-2b---lt2---ruang-dalam-1",
+      "name": "2B - LT2 - RUANG DALAM 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.5216595208738593,
+        "pitch": 0.24115352279369162,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8250828202370872,
+          "pitch": 0.23124425574374996,
+          "rotation": 0,
+          "target": "6-2a---lt2---ruang-utama"
+        },
+        {
+          "yaw": -0.6865651422126184,
+          "pitch": 0.3044400692250271,
+          "rotation": 0,
+          "target": "8-2c---lt2---balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-2c---lt2---balkon",
+      "name": "2C - LT2 - BALKON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.08726646259971815,
+        "pitch": 0.15541004802260616,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9111169653515354,
+          "pitch": 0.15371404431107294,
+          "rotation": 0,
+          "target": "7-2b---lt2---ruang-dalam-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-2d---lt2---ruang-dalam-2",
+      "name": "2D - LT2 - RUANG DALAM 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.4407276503948232,
+        "pitch": 0.2642028559853671,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2799377405645522,
+          "pitch": 0.17502504125540064,
+          "rotation": 0,
+          "target": "6-2a---lt2---ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-2e---lt2---wc-1",
+      "name": "2E - LT2 - WC 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.4939814508409643,
+        "pitch": -0.03701499866940949,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8914988035513325,
+          "pitch": 0.09895366055359744,
+          "rotation": 0,
+          "target": "6-2a---lt2---ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-2f---lt2---ruang-dalam-3",
+      "name": "2F - LT2 - RUANG DALAM 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.8283478601026424,
+        "pitch": 0.09536932549978161,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8181784204529841,
+          "pitch": 0.12215439391941274,
+          "rotation": 0,
+          "target": "12-2g---lt2---wc-2"
+        },
+        {
+          "yaw": 1.353434359424753,
+          "pitch": 0.13218013773448867,
+          "rotation": 1.5707963267948966,
+          "target": "6-2a---lt2---ruang-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-2g---lt2---wc-2",
+      "name": "2G - LT2 - WC 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.496588545963002,
+        "pitch": 0.2570019267903376,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1400884220650873,
+          "pitch": 0.006231526941000709,
+          "rotation": 0,
+          "target": "11-2f---lt2---ruang-dalam-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "GRIYA AKSANA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
